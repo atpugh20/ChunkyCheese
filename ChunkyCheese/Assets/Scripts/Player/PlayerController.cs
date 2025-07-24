8ee369fe95ae239d3fc82bb65d4f0ed9fd2a2e36
Alex <133706872+atpugh20@users.noreply.github.com>
2025-07-24 01:45:20 -0700

    implement movement, jump, and double jump

--- ChunkyCheese/Assets/Scripts/Player/PlayerController.cs
+++ ChunkyCheese/Assets/Scripts/Player/PlayerController.cs
@@ -3,27 +3,57 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
 
+    #region Serialized Fields
+
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private LayerMask _groundLayer;
+
+    #endregion
+
+    #region Input Actions
 
     private InputAction _moveAction;
+    private InputAction _jumpAction;
+
+    #endregion    
+
     private Rigidbody2D _rb;
 
     private Vector2 _moveValue;
+    private bool _isGrounded;
+    private bool _jumpRemaining;
 
 
-
-    void Start() {
+    void Start()
+    {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
         _rb = GetComponent<Rigidbody2D>();
+
+        _isGrounded = true;
     }
 
-    void Update() {
-        _moveValue = _moveAction.ReadValue<Vector2>(); 
-    }
+    void Update()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
 
-    private void FixedUpdate() {
-        Vector2 movement = _moveValue * _moveSpeed * Time.deltaTime;
-        Vector3 moveTowards = new Vector3(movement.x, 0, 0) + transform.position;
-        _rb.MovePosition(moveTowards);
-    }
+        if (_isGrounded)
+            _jumpRemaining = true;
+
+        _moveValue = _moveAction.ReadValue<Vector2>();
+        _moveValue *= _moveSpeed * Time.deltaTime;
+
+        _rb.linearVelocityX = _moveValue.x;
+
+        if (_jumpAction.WasPerformedThisFrame() && (_isGrounded || _jumpRemaining))
+        {
+            if (!_isGrounded)
+                _jumpRemaining = false;
+
+            _rb.linearVelocityY = _jumpSpeed * Time.deltaTime;
+        }
+    } 
 }
